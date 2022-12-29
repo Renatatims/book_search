4370de2457a3a1ad1c9ef2eb39f4e6eaebc493a7
ReTims <renatatims@gmail.com>
2022-12-29 16:37:48 -0600

    Const resolvers - defined query and mutation to work with mongoose models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,21 +1,48 @@
-const {User} = require('../models');
+//import User model
+const { User } = require("../models");
+// import sign token function from auth
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        me: async () => {
-            return await User.findOne({_id: User._id});
-        },
-
+  Query: {
+    me: async () => {
+      return await User.findOne({ _id: User._id });
     },
-     Mutation: {
-            createUser: async (parent, args) => {
-                return await User.create(args);
-            }
-            //login
+  },
+  Mutation: {
+    // Create a User
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
     },
-    //saveBook
-    //deleteBook
-        
- }
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
- module.exports = resolvers;
+      const correctPw = await user.isCorrectPassword(password);
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    // Save a book
+    saveBook: async (parent, { bookInput }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: User._id },
+        { $addToSet: { savedBooks: bookInput } },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    },
+    //Delete a book
+    removeBook: async (parent, { bookId }) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId} } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  },
+};
+
+module.exports = resolvers;
