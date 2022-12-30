c0476c05f4c828be5f4375d9c0333f20f0610280
ReTims <renatatims@gmail.com>
2022-12-29 20:04:58 -0600

    Created doc queries.js + included query me

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+query me {
+    _id
+    username
+    email
+    savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+    }
+}`;
